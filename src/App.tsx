937bb142b349c2701153d596dae00bd1de055c15
Jason Power <jrpower@gmail.com>
2020-05-12 20:44:36 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
           />
           <StandardButton clickHandler = { (e) =>{console.log('clicked')}}/>
         </div>
-        
+        test
         <a
           className="App-link"
           href="https://reactjs.org"
